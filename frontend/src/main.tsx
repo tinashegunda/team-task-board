ff2dea54b754a821ac7aef4d8f561179cb5d819d
Tinashe Gunda <gunda.tinashe@gmail.com>
2026-08-19 21:36:12 +0200

    feat(frontend): add mui theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import App from './App.tsx'
+import { theme } from './theme.ts'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
